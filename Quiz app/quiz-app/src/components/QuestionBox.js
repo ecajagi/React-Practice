1ad8ac3bba7dfed44eba193a8bdf8fe24f019e58
Erik Čajagi <73696957+ecajagi@users.noreply.github.com>
2021-08-11 15:33:58 +0200

    initial upload

---
+++ Quiz app/quiz-app/src/components/QuestionBox.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+const QuestionBox = ({question, options, selected}) => {
+  //we use this hook to update the value of the answer variable and we use a function component here as we only display data
+  const [answer, setAnswer] = useState(options);
+  return (
+    <div className="questionBox">
+      <div className="question">{question}</div>
+      {answer.map((text, index) => (
+        <button
+          key={index}
+          className="answerBtn"
+          onClick={() => {
+            setAnswer([text]);
+            selected(text);
+          }}
+        >
+          {text}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionBox;
